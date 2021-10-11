67e8b6b2264096f6748fc6905f66f2ca96120830
Matias Senger <m.senger@hotmail.com>
2021-10-11 16:33:20 +0200

    Implemented set_trig_coupling

--- TeledyneLeCroyPy/__init__.py
+++ TeledyneLeCroyPy/__init__.py
@@ -2,10 +2,19 @@ import time
 import numpy as np
 from pyvisa.resources import Resource
 
+
+
 def _validate_channel_number(channel):
 	CHANNEL_NUMBERS = {1,2,3,4}
 	if channel not in CHANNEL_NUMBERS:
 		raise ValueError(f'<channel> must be in {CHANNEL_NUMBERS}')
+
+def _validate_trig_source(trig_source):
+	TRIG_SOURCES_VALID = {'C1','C2','C3','C4','Ext','Line','FastEdge'}
+	if not isinstance(trig_source, str):
+		raise TypeError(f'The trigger source must be a string, received object of type {type(trig_source)}.')
+	if trig_source.lower() not in {t.lower() for t in TRIG_SOURCES_VALID}:
+		raise ValueError(f'The trigger source must be one of {TRIG_SOURCES_VALID}, received {repr(trig_source)}...')
 
 class LeCroyWaveRunner:
 	def __init__(self, instrument):
@@ -109,15 +118,24 @@ class LeCroyWaveRunner:
 		return str(self.query("VBS? 'return=app.Acquisition.Trigger.Source'"))
 	
 	def set_trig_source(self, source: str):
-		"""Sets the trigger source."""
+		"""Sets the trigger source (C1, C2, Ext, etc.)."""
 		# See http://cdn.teledynelecroy.com/files/manuals/automation_command_ref_manual_ws.pdf#page=34
-		if not isinstance(source, str):
-			raise TypeError(f'<source> must be a string, received object of type {type(source)}.')
-		allowed_trigger_sources = {'C1','C2','C3','C4','Ext','Line','FastEdge'}
-		if source.lower() not in {t.lower() for t in allowed_trigger_sources}:
-			raise ValueError(f'<source> must be one of {allowed_trigger_sources}, received {repr(source)}...')
+		_validate_trig_source(source)
 		string = "VBS 'app.Acquisition.Trigger.Source = "
 		string += '"' + source + '"'
+		string += "'"
+		print(string)
+		self.write(string)
+	
+	def set_trig_coupling(self, trig_source: str, trig_coupling: str):
+		"""Set the trigger coupling (DC, AC, etc.)."""
+		# See http://cdn.teledynelecroy.com/files/manuals/automation_command_ref_manual_ws.pdf#page=37
+		_validate_trig_source(trig_source)
+		VALID_TRIG_COUPLINGS = {'AC','DC','HFREJ','LFREJ'}
+		if not isinstance(trig_coupling, str) or trig_coupling.lower() not in {tc.lower() for tc in VALID_TRIG_COUPLINGS}:
+			raise ValueError(f'The trigger coupling must be one of {VALID_TRIG_COUPLINGS}, received {repr(trig_coupling)}...')
+		string = f"VBS 'app.Acquisition.Trigger.{trig_source}.Coupling = "
+		string += '"' + trig_coupling + '"'
 		string += "'"
 		print(string)
 		self.write(string)
@@ -134,3 +152,4 @@ if __name__ == '__main__':
 	
 	osc = LeCroyWaveRunner(pyvisa.ResourceManager().open_resource('USB0::0x05FF::0x1023::4751N40408::INSTR'))
 	print(osc.idn)
+	osc.set_trig_coupling('ext', 'DC')
